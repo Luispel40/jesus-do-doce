26ae4ba996bef59938ebe78aa7797941ec18ba95
Luispel40 <luispel2011@gmail.com>
2025-02-11 20:15:56 -0300

    add toast and swipe logo

--- script.js
+++ script.js
@@ -15,3 +15,50 @@ accordionTitles.forEach((title) => {
   title.addEventListener("click", openThis);
 });
 
+
+let notifications = document.querySelector('.notifications');
+let warning = document.getElementById('warning');
+
+function createToast(type, icon, title, text) {
+  let newToast = document.createElement('div');
+  newToast.innerHTML = `
+            <div class="toast ${type}">
+                <i class="${icon}"></i>
+                <div class="content">
+                    <div class="title">${title}</div>
+                    <span>${text}</span>
+                </div>
+                <i class="fa-solid fa-xmark" onclick="(this.parentElement).remove()"></i>
+            </div>`;
+  notifications.appendChild(newToast);
+  newToast.timeOut = setTimeout(
+    () => newToast.remove(), 5000
+  )
+}
+
+window.onload = function () {
+
+  let type = 'warning';
+  let icon = 'fa-solid fa-triangle-exclamation';
+  let title = 'Aviso importante!';
+  let text = 'Restam poucas vagas!';
+  createToast(type, icon, title, text);
+
+
+  setInterval(() => {
+    let type = 'warning';
+    let icon = 'fa-solid fa-triangle-exclamation';
+    let title = 'Aviso importante!';
+    let text = 'Mais pessoas estão visualizando essa página!';
+    createToast(type, icon, title, text);
+  }, 20000)
+}
+
+warning.onclick = function () {
+  let type = 'warning';
+  let icon = 'fa-solid fa-triangle-exclamation';
+  let title = 'Aviso importante!';
+  let text = 'Restam poucas vagas!';
+  createToast(type, icon, title, text);
+}
+
